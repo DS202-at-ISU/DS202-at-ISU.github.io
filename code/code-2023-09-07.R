4f08b0cf0ff2865ae8101ac1f37213542ec576e7
willju-wangqian <wju@iastate.edu>
2023-09-11 18:03:42 -0500

    update schedule for sep 12

--- code/code-2023-09-07.R
+++ code/code-2023-09-07.R
@@ -3,19 +3,64 @@ library(tidyverse)
 
 ?fbi
 
-head(fbi)
+fbi_first10 <- head(fbi, n = 10)
 tail(fbi)
 dim(fbi)
 
 summary(fbi)
 str(fbi)
 
-fbi$population  # this is an object too!
+# this is a numeric vector
+fbi$population  
 
-fbiwide
+# this is a dataframe with one column
+fbi[ , 'population']
 
+?fbiwide
+dim(fbiwide)
+str(fbiwide)
 
+# version 1
+ggplot(data = fbiwide, 
+       aes(x = burglary, y = homicide)) +
+  geom_point() + 
+  geom_smooth()
+# version 2
+fbiwide %>% 
+  ggplot(aes(x = burglary, y = homicide)) +
+  geom_point() +
+  geom_smooth()
 
+ggplot(data = fbiwide, 
+       aes(x = log(burglary), 
+           y = log(homicide))) +
+  geom_point()
+
+ggplot(data = fbiwide, 
+       aes(x = log(burglary), 
+           y = log(motor_vehicle_theft))) +
+  geom_point()
+
+ggplot(aes(x = log(burglary), 
+           y = log(motor_vehicle_theft),
+           colour=state), 
+       data=fbiwide) + 
+  geom_point()
+
+ggplot(aes(x = log(burglary), 
+           y = log(motor_vehicle_theft),
+           colour=factor(year)), 
+       data=fbiwide) + 
+  geom_point()
+
+ggplot(aes(x = log(burglary), 
+           y = log(motor_vehicle_theft),
+           size=population, 
+           color = year,
+           shape = state), 
+       data=fbiwide) + 
+  geom_point() +
+  theme(legend.position = "none")
 
 
 
@@ -31,9 +76,10 @@ fbi %>%
   head() %>% 
   within({
     state = factor(state, levels = state)
-  }) %>% 
+  }) %>%
   ggplot() +
-  geom_bar(aes(x = state, y = total_count), stat = 'identity')
+  geom_bar(aes(x = state, y = total_count), 
+           stat = 'identity')
 
 # Is population correlated to the number of crime?
 fbi %>% 
