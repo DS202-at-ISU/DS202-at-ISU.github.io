21cb6ec85c499b1a2a3dc8fbf4b0ac0e058d04d6
willju-wangqian <wju@iastate.edu>
2023-10-10 14:02:04 -0500

    add code for oct 10

--- code/code-2023-10-10.R
+++ code/code-2023-10-10.R
@@ -7,20 +7,62 @@ deaths <- av %>%
   pivot_longer(starts_with("Death"),
                names_to = "Time",
                values_to = "Died") %>% 
+  select(URL, Name.Alias, Time, Died)
+
+# deaths$Time <- parse_number(deaths$Time)
+maxdeaths <- deaths %>% 
+  mutate(
+    Time = parse_number(Time)
+  ) %>% 
+  filter(Died != '') %>% 
+  group_by(URL, Died) %>% 
+  summarise(
+    Time = max(Time)
+  )
+
+# if we want to include all steps into one pipeline
+maxdeaths <- av %>% 
+  pivot_longer(starts_with("Death"),
+               names_to = "Time",
+               values_to = "Died") %>% 
   select(URL, Name.Alias, Time, Died) %>% 
   mutate(
     Time = parse_number(Time)
   ) %>% 
-  filter(Died != '')
-
-maxdeaths <- deaths %>% group_by(URL, Died) %>% 
+  filter(Died != '') %>% 
+  group_by(URL, Died) %>% 
   summarise(
-    Time = max(Time), .groups = 'drop'
+    Time = max(Time)
   )
 
-maxdeaths %>% select(-URL) %>% 
-  group_by(Died, Time) %>% tally()
+maxdeaths %>% 
+  ungroup() %>% 
+  group_by(Died, Time) %>% 
+  tally()
 
+
+####
 url <- "https://github.com/ds202-at-ISU/materials/blob/master/03_tidyverse/data/ames-liquor.rds?raw=TRUE"
 download.file(url, "data/ames-liquor.rds", mode="wb")
 ames <- readRDS("data/ames-liquor.rds")
+
+ames$Date %>% head()
+
+ames$`Store Location` %>% head()
+ames$`Store Location` %>% head() %>% parse_number()
+
+ames %>% head() %>% 
+  separate(`Store Location`, 
+           into = c("prefix", "lat", 'long'),
+           sep = ' ') %>% 
+  select(prefix, lat, long) %>% 
+  mutate(
+    lat = parse_number(lat),
+    long = parse_number(long)
+  )
+
+
+
+
+
+
