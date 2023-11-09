64fc9babdb2588227e3512e217ab2020c7a85140
willju-wangqian <wju@iastate.edu>
2023-11-09 00:21:10 -0600

    upload code for nov 9

--- code/code-2023-11-07.R
+++ code/code-2023-11-07.R
@@ -4,24 +4,39 @@ library(Lahman)
 
 HallOfFame_players <- HallOfFame %>% 
   filter(yearID == 2018,
-         votedBy == 'BBWAA',
-         votes > 317) %>% 
+         inducted == 'Y',
+         votedBy == 'BBWAA') %>% 
   pull(playerID)
 
-People %>% filter(
-  playerID %in% HallOfFame_players
-)
+People %>% 
+  filter(playerID %in% HallOfFame_players)
 
-df1 <- data.frame(id = 1:6, trt = rep(c("A", "B", "C"), rep=c(2,1,3)), value = c(5,3,7,1,2,3))
-df1
-
-df2 <- data.frame(id=c(4,4,5,5,7,7), stress=rep(c(0,1), 3), bpm = c(65, 125, 74, 136, 48, 110))
-df2
-
+###
 joined_data <- left_join(HallOfFame, People,
                          by = 'playerID')
+joined_data <- HallOfFame %>% 
+  left_join(People, by = 'playerID')
+
 joined_data %>% 
-  filter(votes > needed,
+  filter(inducted == 'Y',
          deathYear > 2018 | is.na(deathYear))
-  
+
+###
+joined_data %>% 
+  filter(nameLast == 'Sosa')
+
+###
+counties <- map_data("county")
+counties %>% 
+  ggplot(aes(x = long, y = lat, group = group)) +
+  geom_path()
+
+counties %>% 
+  ggplot(aes(x = long, y = lat, group = group)) +
+  geom_polygon()
+
+counties %>% 
+  ggplot() +
+  geom_polygon(aes(x = long, y = lat, group = group))
+
 
