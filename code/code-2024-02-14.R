985919fc09def4fa352984a303e1a8535a3d4aa7
willju-wangqian <wju@iastate.edu>
2024-02-14 12:06:44 -0600

    change schedule for feb 14

---
+++ code/code-2024-02-14.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(classdata)
+
+data("mpg")
+str(mpg)
+
+# scatter plot: cty vs hwy
+
+# scatter plot: cty vs hwy, color by manufacturer
+
+# side-by-side boxplot of cty, by manufacturer
+
+# histogram of cty
+
+
