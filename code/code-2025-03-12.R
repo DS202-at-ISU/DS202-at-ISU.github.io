24eeea94d7e66615aca92d4dd19e1c440edd3f20
willju-wangqian <wju@iastate.edu>
2025-03-12 13:40:15 -0500

    mar 12 code

--- code/code-2025-03-12.R
+++ code/code-2025-03-12.R
@@ -1,126 +1,85 @@
 library(tidyverse)
 
-data(french_fries, package="reshape2")
-str(french_fries)
-View(french_fries)
-
-# pivot_longer
-df1 <- data.frame(
-  treatment = c('a', 'b'),
-  John.Smith = c(NA, 18),
-  Jane.Doe = c(4, 1),
-  Mary.Johnson = c(6, 7)
-)
-df1 %>% pivot_longer(
-  John.Smith:Mary.Johnson,
-  names_to = "patient",
-  values_to = "score"
-)
-
-# your turn
-av <- read.csv("https://raw.githubusercontent.com/fivethirtyeight/data/master/avengers/avengers.csv", stringsAsFactors = FALSE)
-View(av)
-
-av %>% 
-  select(
-    Name.Alias,
-    starts_with("Death")
-  ) %>% 
-  head()
-
-deaths <- av %>% 
-  pivot_longer(
-    starts_with("Death"),
-    names_to = "Time",
-    values_to = "Died"
-  ) %>% 
-  select(
-    URL, Name.Alias, Time, Died
-  )
-head(deaths)
-View(deaths)
-
-deaths %>% count(Died)
-deaths %>% group_by(Died) %>% tally()
-deaths %>% group_by(Died) %>% summarise(n = n())
-table(deaths$Died)
-
-# pivot_wider
 data <- data.frame(
   treatment = c("a", "b", "a", "b", "a", "b"),
   patient = c("John Smith", "John Smith", "Jane Doe", "Jane Doe", "Mary Johnson", "Mary Johnson"),
   score = c(NA, 18, 4, 1, 6, 7)
 )
 
-data %>% 
-  pivot_wider(
-    names_from = patient, 
-    values_from = score
-  ) %>% View()
-
-data %>% 
-  pivot_wider(
-    names_from = treatment, 
-    values_from = score,
-    names_prefix = "treatment_"
-  ) %>% View()
-
-# your turn
-french_fries %>% 
-  pivot_longer(
-    potato:painty,
-    names_to = "scale",
-    values_to = "score"
-  ) %>% 
-  pivot_wider(
-    names_from = "time",
-    values_from = "score"
-  ) %>% 
-  ggplot(aes(x = `1`, y = `10`,
-             color = subject,
-             shape = factor(rep))) +
-  geom_point() +
-  facet_grid(treatment ~ scale)
-
-# fbiwide
-library(classdata)
-fbilong <- fbiwide %>% 
-  pivot_longer(
-    homicide:arson,
-    names_to = "Type",
-    values_to = "Incidences"
-  ) %>% 
-  mutate(
-    rate = Incidences / population * 100000
-  )
-
-fbilong %>% 
-  filter(state_abbr %in% c("IA", "MN")) %>% 
-  select(state, year, Type, rate) %>% 
-  pivot_wider(
-    names_from = state,
-    values_from = rate
-  ) %>% 
-  ggplot(aes(x = Iowa, y = Minnesota,
-             color = Type)) +
-  geom_point() +
-  geom_abline() +
-  facet_wrap(~Type,
-             scales = "free")
-
-fbilong %>% 
-  filter(state_abbr %in% c("IA", "MN")) %>% 
-  select(state, rate) %>%
-  pivot_wider(
-    names_from = state,
-    values_from = rate
-  ) -> tt
-
-#
 data$unique_id <- 1:6
 data %>%
-  select(-unique_id) %>% 
+  select(-unique_id) %>%
   pivot_wider(
     names_from = treatment,
     values_from = score
   )
+
+library(classdata)
+fbi %>% filter(year==2014, 
+               state_abbr %in% c("IA", "MN"),
+               type=="burglary") %>% 
+  select(-state_id, -state_abbr,
+         -population) %>% 
+  pivot_wider(
+    names_from = "state",
+    values_from = "count"
+  ) 
+
+View(deaths)
+maxdeaths <- deaths %>% 
+  mutate(
+    Time = parse_number(Time)
+  ) %>% 
+  group_by(URL, Died) %>% 
+  summarise(
+    total_death = max(Time)
+  ) %>% 
+  filter(Died != "")
+  
+maxdeaths %>% 
+  ungroup() %>% 
+  count(Died, total_death)
+
+maxdeaths %>% 
+  group_by(Died, total_death) %>% 
+  tally()
+
+
+# url <- "https://github.com/ds202-at-ISU/materials/blob/master/03_tidyverse/data/ames-liquor.rds?raw=TRUE"
+# download.file(url, "ames-liquor.rds", mode="wb")
+ames <- readRDS("data/ames-liquor.rds")
+
+tt <- ames %>% head()
+parse_number(tt$`Store Location`)
+ames_cleaned <- ames %>% 
+  separate(
+    `Store Location`,
+    into = c("prefix", "long", "lat"),
+    sep = ' '
+  ) %>% 
+  mutate(
+    lat = parse_number(lat),
+    long = parse_number(long)
+  )
+ames_cleaned %>% View()
+sum(ames_cleaned$`Sale (Dollars)`, na.rm = TRUE)
+max(ames_cleaned$`Sale (Dollars)`, na.rm = TRUE)
+max(ames_cleaned$`Volume Sold (Gallons)`, na.rm = TRUE)
+
+ames_cleaned %>% 
+  ggplot(aes(x = long, y = lat)) +
+  geom_point()
+
+mean_long <- mean(ames_cleaned$long, na.rm = TRUE)
+mean_lat <- mean(ames_cleaned$lat, na.rm = TRUE)
+
+library(ggmap)
+# ggmap::register_google() with your API first
+# see ?ggmap::register_google for details
+ames_map = get_map(location = c(lon = mean_long, 
+                           lat = mean_lat), 
+              zoom = 13, maptype = 'roadmap')
+ggmap(ames_map) +
+  geom_point(data = ames_cleaned,
+             aes(x = long, y = lat),
+             size = 0.1, alpha = 0.1)
