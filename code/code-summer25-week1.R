140c69aef57520cfefece42dba971822a0703d38
willju-wangqian <wju@iastate.edu>
2025-08-25 01:42:07 -0500

    update Fall25 schedule

---
+++ code/code-summer25-week1.R
@@ -0,0 +1,68 @@
+1 + 1
+2 + 2
+print("hello ISU")
+
+10 * 2
+
+print("hello Ames")
+sum(c(1,3,4))
+x <- 1
+x
+
+x <- 2/3
+sqrt(x)
+
+y <- c(1, 4, 5, 2)
+sum(y)
+
+y[2]
+y[1]
+y[3]
+
+# sum of first and third elements in y
+y[1] + y[3]
+
+y[c(1, 3)]
+y[c(1, 3, 4)]
+2 * y
+
+x <- c(4, 1, 3, 9 )
+y <- c(1, 2, 3, 5)
+x - y
+(x - y)^2
+sum((x - y)^2)
+sqrt(sum((x - y)^2))
+
+sqrt(sum((x - y)^2))
+
+bool_vec <- c(TRUE, FALSE)
+
+mtcars
+head(mtcars)
+
+?sum
+?mtcars
+?head
+
+install.packages("remotes")
+remotes::install_github("heike/classdata")
+
+library(classdata)
+?fbi
+fbi
+
+1:5
+1:100
+fbi[c(1,5,6), c(3,4,5)]
+
+fbi$population
+head(fbi)
+?head
+summary(fbi)
+str(fbi)
+dim(fbi)
+
+str(mtcars)
+
+fbi$population
+mean(fbi$population)
