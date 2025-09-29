3818551440c0e3ee2646c501fc038d31c0fe16a0
willju-wangqian <wju@iastate.edu>
2025-09-28 22:42:42 -0500

    update schedule for sep 29

---
+++ code/code-2025-09-29.R
@@ -0,0 +1,15 @@
+library(classdata)
+library(tidyverse)
+
+# happy_map <- list(
+#   "not too happy" = 1,
+#   "pretty happy" = 2,
+#   "very happy" = 3
+# )
+# 
+# happy |> 
+#   mutate(
+#     nhappy = sapply(happy, function(e) happy_map[[e]])
+#   ) |> 
+#   select(happy, nhappy) |> 
+#   View()
