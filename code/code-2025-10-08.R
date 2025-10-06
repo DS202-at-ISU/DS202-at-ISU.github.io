75a82e55bad9ff9b96d27dd159c315ebba3cb4bd
willju-wangqian <wju@iastate.edu>
2025-10-06 15:01:50 -0500

    add code oct 08

---
+++ code/code-2025-10-08.R
@@ -0,0 +1,13 @@
+library(here)
+
+df1 <- data.frame(
+  name = c("John Smith", "Jane Doe", "Mary Johnson"),
+  treatmenta = c(NA, 4, 6),
+  treatmentb = c(18, 1, 7)
+)
+df2 <- data.frame(
+  treatment = c("a", "b"),
+  John.Smith = c(NA, 18),
+  Jane.Doe = c(4, 1),
+  Mary.Johnson = c(6, 7)
+)
